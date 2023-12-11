2e3f08f895fa9a3bbbb1a5749e50d6216ecbcafe
joester030376 <firehawkdigitalohio@gmail.com>
2023-12-11 18:19:58 -0500

    added coding files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// routes
+import Router from "./routes";
+// theme
+import ThemeProvider from './theme';
+// components
+import ThemeSettings from './components/settings';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <ThemeSettings>
+        {" "}
+        <Router />{" "}
+      </ThemeSettings>
+    </ThemeProvider>
   );
 }
 
