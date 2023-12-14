e06e5d24c98d772f53a5a45579f8de8edeb0ce31
joester030376 <firehawkdigitalohio@gmail.com>
2023-12-13 21:13:27 -0500

    finished the sidebar component.

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,11 +1,54 @@
 import React, {useState} from "react";
-import { Box, Stack, IconButton, Divider } from "@mui/material";
+import { Box, Stack, IconButton, Divider, Avatar, Switch } from "@mui/material";
 import { Outlet } from "react-router-dom";
-import { useTheme } from "@mui/material/styles";
+import { useTheme, styled } from "@mui/material/styles";
 import appimages from "../../assets/Images/AppImages";
-import { Gear } from "phosphor-react";
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import { Nav_Buttons } from "../../data";
+import { faker } from "@faker-js/faker";
+import useSettings from "../../hooks/useSettings";
 
+const AntSwitch = styled(Switch)(({ theme }) => ({
+  width: 40,
+  height: 20,
+  padding: 0,
+  display: 'flex',
+  '&:active': {
+    '& .MuiSwitch-thumb': {
+      width: 15,
+    },
+    '& .MuiSwitch-switchBase.Mui-checked': {
+      transform: 'translateX(9px)',
+    },
+  },
+  '& .MuiSwitch-switchBase': {
+    padding: 2,
+    '&.Mui-checked': {
+      transform: 'translateX(20px)',
+      color: '#fff',
+      '& + .MuiSwitch-track': {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === 'dark' ? '#177ddc' : '#1890ff',
+      },
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    boxShadow: '0 2px 4px 0 rgb(0 35 11 / 20%)',
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+    transition: theme.transitions.create(['width'], {
+      duration: 200,
+    }),
+  },
+  '& .MuiSwitch-track': {
+    borderRadius: 20 / 2,
+    opacity: 1,
+    backgroundColor:
+      theme.palette.mode === 'dark' ? 'rgba(255,255,255,.35)' : 'rgba(0,0,0,.25)',
+    boxSizing: 'border-box',
+  },
+}));
 
 const DashboardLayout = () => {
 
@@ -14,6 +57,8 @@ const DashboardLayout = () => {
   const [selected, setSelected] = useState(0);
 
   console.log(theme);
+
+  const {onToggleMode} = useSettings();
 
   return (
     <>
@@ -27,57 +72,100 @@ const DashboardLayout = () => {
             width: 100,
           }}
         >
-          <Stack direction={"column"} sx={{width: "100%"}} alignItems={"center"} spacing={3} >
-              <Box sx={{
-                backgroundColor: theme.palette.primary.main,
-                height: 64,
-                width: 64,
-                borderRadius: 1.5,
-              }}>
-                  <img src={appimages.images.logo} alt="chat app logo" />  
-
-              </Box> 
-              <Stack 
-                spacing={3} 
-                sx={{
-                  width: "max-content"
-                }}
-                  direction="column"
-                  alignItems="center"
-                >
-                {Nav_Buttons.map((el) => (
-                  el.index === selected ?
-                  <Box 
-                    sx={{
-                      backgroundColor: theme.palette.primary.main,
-                      borderRadius: 1.5
-                    }}
-                  >
-                    <IconButton key={el.index}
+            <Stack 
+              direction={"column"}
+              sx={{width: "100%", height: "100%"}} 
+              justifyContent={"space-between"}
+              alignItems={"center"} 
+              spacing={3} 
+            >
+              <Stack alignItems={"center"} spacing={4}>
+                <Box sx={{
+                  backgroundColor: theme.palette.primary.main,
+                  height: 64,
+                  width: 64,
+                  borderRadius: 1.5,
+                }}>
+                    <img src={appimages.images.logo} alt="chat app logo" />  
+                </Box> 
+                <Stack 
+                  spacing={3} 
+                  sx={{width: "max-content"}} 
+                  direction="column" 
+                  alignItems="center">
+                  {Nav_Buttons.map((el) => (  
+                    el.index === selected ?
+                    <Box
+                      p={1}
+                      sx={{
+                        backgroundColor: theme.palette.primary.main,
+                        borderRadius: 1.5
+                      }}
+                    >
+                      <IconButton key={el.index}
+                        sx={{
+                          width: "max-content",
+                          color: "#fff"
+                        }}
+                      >
+                        {el.icon}
+                      </IconButton> 
+                    </Box>          
+                    : <IconButton 
+                        onClick={() => {
+                          setSelected(el.index)
+                        }}
+                        key={el.index}
+                        sx={{
+                          width: "max-content",
+                          color: theme.palette.mode === 'light' ? "#000" : theme.palette.text.primary
+                        }}
+                      >
+                        {el.icon}
+                      </IconButton>               
+                  ))}
+                  <Divider flexItem />   
+                  { selected === 3 ? 
+                    <Box
+                      p={1}
+                      sx={{                     
+                        backgroundColor: theme.palette.primary.main,
+                        borderRadius: 1.5
+                      }}
+                    >
+                      <IconButton
+                        sx={{
+                          width: "max-content",
+                          color: "#fff"
+                        }}
+                      >
+                        <SettingsOutlinedIcon />
+                      </IconButton>  
+                  </Box>
+                  :
+                    <IconButton                  
+                      onClick={() => {
+                        setSelected(3);
+                      }}  
                       sx={{
                         width: "max-content",
-                        color: "#fff"
-                      }}                    
+                        color: theme.palette.mode === 'light' ? "#000" : theme.palette.text.primary
+                      }}                  
                     >
-
-                      {el.icon}
-                    </IconButton>
-                  </Box> 
-                 : <IconButton key={el.index}
-                  sx={{
-                    width: "max-content",
-                    color: "#000"
-                  }}                    
-                >
-                  {el.icon}
-                </IconButton>                 
-              ))}
-              <Divider />  
-              <IconButton>
-                <Gear size={32} />
-              </IconButton>
-              </Stack>
-              
+                      <SettingsOutlinedIcon />
+                    </IconButton> 
+                  }                           
+                </Stack> 
+              </Stack>  
+              <Stack spacing={4}>
+                <AntSwitch 
+                  defaultChecked 
+                  onChange={() => {
+                    onToggleMode();
+                  }}
+                />
+                <Avatar src={faker.image.avatar()}    />   
+              </Stack>                   
             </Stack>       
         </Box>
      
